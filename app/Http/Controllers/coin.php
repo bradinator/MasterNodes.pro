15a2a774bf619e1e62a96969d270544a378e9994
Yoshi <jason@htmllabs.com>
2017-07-18 17:09:23 -0400

    Major Update

--- app/Http/Controllers/coin.php
+++ app/Http/Controllers/coin.php
@@ -16,20 +16,110 @@ class coin extends Controller
 		$data     = null;
 		$coinList = $this->coinList();
 		foreach ($coinList as $one) {
-			$data['coinList'][$one['coin']] = json_decode(Storage::get('' . $one['coin'] . '.json'), true);
-			$data['coinList'][$one['coin']]['logo'] = $one['logo'];
+			$data['coinList'][$one['coin']]             = json_decode(Storage::get('' . $one['coin'] . '.json'), true);
+			$data['coinList'][$one['coin']]['roi']      = '';
+			$data['coinList'][$one['coin']]['logo']     = $one['logo'];
+			$data['coinList'][$one['coin']]['donation'] = $one['donation'];
 		}
+		$data['ComingSoonCoinList'] = $this->ComingSoonCoinList();
+		$data['donateCoinList']     = $this->donateCoinList();
 		return view('welcome', $data);
+	}
+
+	public function getBalance($donate)
+	{
+		$client = new Client();
+		$total  = 0;
+		foreach ($donate as $key => $value) {
+			if ($key === 'bitcoin') {
+				$res        = $client->request(
+					'GET', 'https://blockchain.info/q/getreceivedbyaddress/' . $value
+				);
+				$contentCMC = (float)$res->getBody()->getContents();
+				$total      = $total + ($contentCMC / 100000000);
+			} else {
+				$url       = 'http://chainz.cryptoid.info/' . $key . '/api.dws?q=ticker.btc';
+				$res       = $client->request(
+					'GET', $url
+				);
+				$tickerBTC = (float)$res->getBody()->getContents();
+				$url       = 'http://chainz.cryptoid.info/' . $key . '/api.dws?q=getreceivedbyaddress&a=' . $value;
+				$res       = $client->request(
+					'GET', $url
+				);
+				$cointotal = (float)$res->getBody()->getContents();
+				$coin2btc  = number_format($cointotal * $tickerBTC, '8', '.', '');
+				$total     = $total + $coin2btc;
+			}
+		}
+		return $total;
+	}
+
+	public function ComingSoonCoinList()
+	{
+
+		// DASH COIN
+		$coin['coin'] = 'NTRN';
+		$coin['url']  = 'http://www.neutroncoin.com';
+		$coin['logo'] = 'https://static.wixstatic.com/media/f2591a_f17f4b3fcbb74848b2bccf59bbeae490~mv2.png/v1/fill/w_708,h_520,al_c,lg_1/f2591a_f17f4b3fcbb74848b2bccf59bbeae490~mv2.png';
+		$coins[0]     = $coin;
+		$coin['coin'] = 'INSN';
+		$coin['url']  = 'http://www.insanecoin.com/';
+		$coin['logo'] = 'https://files.coinmarketcap.com/static/img/coins/32x32/insanecoin-insn.png';
+		$coins[1]     = $coin;
+		foreach ($coins as $one) {
+			$data[$one['coin']] = $one;
+		}
+		return $data;
+	}
+
+	public function donateCoinList()
+	{
+		$client     = new Client();
+		$resCMCCORE = $client->request(
+			'GET', 'https://blockchain.info/ticker'
+		);
+		$ticker     = json_decode($resCMCCORE->getBody()->getContents(), true);
+		// DASH COIN
+		$coin['coin']              = 'DASH';
+		$coin['url']               = 'https://www.dash.org/';
+		$coin['logo']              = '/img/dash_square_bevel_highres.png';
+		$coin['donate']['bitcoin'] = '19t1VRFe5MrWCFeZFruwnPJQACdTs82ytx';
+		$coin['donate']['dash']    = 'XqF2WcDgktmTKJ1j8Frn7AqkzfVWHZxykm';
+		$coin['current']           = (float)$this->getBalance($coin['donate']) * $ticker['USD']['15m'];
+		$coin['need']              = 200;
+		$coin['balance']           = $coin['need'] - $coin['current'];
+		$coins[0]                  = $coin;
+		$coin                      = [];
+		$coin['coin']              = 'arcticcoin';
+		$coin['url']               = 'https://arcticcoin.org/';
+		$coin['logo']              = 'https://arcticcoin.org/main/images/logo.png';
+		$coin['donate']['bitcoin'] = '1MMWFtENNefjTxch1j6X3MyCEQ2u9AhZSf';
+		$coin['donate']['dash']    = 'APzt8HxZiqJCRsWJYSNFj8zXdNT4LYwDoH';
+		$coin['current']           = (float)$this->getBalance($coin['donate']) * $ticker['USD']['15m'];
+		$coin['need']              = 200;
+		$coin['balance']           = $coin['need'] - $coin['current'];
+		$coins[1]                  = $coin;
+		foreach ($coins as $one) {
+			$data[$one['coin']] = $one;
+		}
+		return $data;
 	}
 
 	public function coinList()
 	{
-		$data[0]['coin'] = 'ion';
-		$data[0]['logo'] = '//ion.masternodes.pro/img/logo.png';
-		$data[1]['coin'] = 'chc';
-		$data[1]['logo'] = '//chc.masternodes.pro/img/logo.png';
-		$data[2]['coin'] = 'pivx';
-		$data[2]['logo'] = 'https://raw.githubusercontent.com/PIVX-Project/Official-PIVX-Graphics/master/digital/bottom%20tag/portrait/White/White_Port.png';
+		$data[0]['coin']                       = 'ion';
+		$data[0]['logo']                       = '//ion.masternodes.pro/img/logo.png';
+		$data[0]['donation']['bitcoin']        = '';
+		$data[0]['donation'][$data[0]['coin']] = '';
+		$data[1]['coin']                       = 'chc';
+		$data[1]['logo']                       = '//chc.masternodes.pro/img/logo.png';
+		$data[1]['donation']['bitcoin']        = '';
+		$data[1]['donation'][$data[1]['coin']] = '';
+		$data[2]['coin']                       = 'pivx';
+		$data[2]['logo']                       = 'https://raw.githubusercontent.com/PIVX-Project/Official-PIVX-Graphics/master/digital/bottom%20tag/portrait/White/White_Port.png';
+		$data[2]['donation']['bitcoin']        = '';
+		$data[2]['donation'][$data[2]['coin']] = '';
 		return $data;
 	}
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,11 @@
 @include('layout.header')
+<style>
+    .popover-title {
+        color: white;
+        background-color: black;
+        font-size: 15px;
+    }
+</style>
 <body>
 @include('layout.sidebar')
 <div class="container-fluid">
@@ -16,6 +23,9 @@
                 <a href="https://www.vultr.com/?ref=6877914"><img src="https://www.vultr.com/media/banner_1.png" width="728" height="90"></a>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+                <br><br><br>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
                 <div class="alert alert-info">Want your Coin Listed here? Email us: <a href="mailto:addme@masternodes.pro">addme@masternodes.pro</a></div>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
@@ -25,6 +35,7 @@
                         <td></td>
                         <td>Coin</td>
                         <td>Total Master Nodes</td>
+                        <td>Coins Locked in MasterNodes</td>
                         <td>Current USD Price</td>
                         <td>Required Coin's</td>
                         <td>What's a Node Worth</td>
@@ -37,9 +48,11 @@
                     </thead>
                     <tbody>
                     @foreach ($coinList as $key => $one)
-                        <tr><td><a href="https://{!! strtoupper($key) !!}.masternodes.pro" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></td>
+                        <tr>
+                            <td><a href="https://{!! strtoupper($key) !!}.masternodes.pro" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></td>
                             <td><a href="https://{!! strtoupper($key) !!}.masternodes.pro" target="_blank">{!! strtoupper($key) !!}</a></td>
                             <td>{!! $one['totalMasterNodes'] !!}</td>
+                            <td>{!! number_format($one['totalMasterNodes'] * $one['masterNodeCoinsRequired'],'0','',',') !!}</td>
                             <td>${!! number_format($one['currentUSDPrice'],2,'.','') !!}</td>
                             <td>{!! $one['masterNodeCoinsRequired'] !!} {!! strtoupper($key) !!}</td>
                             <td>${!! $one['masterNodeWorth'] !!}</td>
@@ -54,6 +67,80 @@
                 </table>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+                <br><br><br>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+                <div class="alert alert-info">Coming Soon.</div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+                <table width="100%">
+                    <thead>
+                    <tr>
+                        <td></td>
+                        <td>Coin</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($ComingSoonCoinList as $key => $one)
+                        <tr>
+                            <td><a href="{!! $one['url'] !!}" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></td>
+                            <td><a href="{!! $one['url'] !!}" target="_blank">{!! strtoupper($key) !!}</a></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+                <br><br><br>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+                <div class="alert alert-info">Help Fund Masternode Detail Site for 1 Year.</div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+                <table width="100%">
+                    <thead>
+                    <tr>
+                        <td></td>
+                        <td>Coin</td>
+                        <td>Required</td>
+                        <td>Balance</td>
+                        <td>Need</td>
+                        <td>Donation Address</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($donateCoinList as $key => $one)
+                        <tr>
+                            <td><a href="{!! $one['url'] !!}" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></td>
+                            <td><a href="{!! $one['url'] !!}" target="_blank">{!! strtoupper($key) !!}</a></td>
+                            <td>${!! number_format($one['need'],'2','.',',') !!}</td>
+                            <td>${!! number_format($one['current'],'2','.',',') !!}</td>
+                            <td>${!! number_format($one['balance'],'2','.',',') !!}</td>
+                            <td>
+                                @foreach ($one['donate'] as $donateKey => $donateOne)
+                                    {!! strtoupper($donateKey) !!}:
+                                    <a href="{!! strtoupper($donateKey) !!}:{!! $donateOne !!}"
+                                       target="_blank"
+                                       data-toggle="popover"
+                                       data-trigger="hover"
+                                       title="{!! strtoupper($donateKey) !!} address"
+                                       data-html="true"
+                                       data-content="<img src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={!! $donateOne !!}' width='150'>"
+                                    >
+                                        {!! $donateOne !!}
+                                    </a>
+                                    <Br>
+                                @endforeach
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+                <br><br><br>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
                 <script data-cfasync=false src="//s.ato.mx/p.js#id=2194065&size=728x90"></script>
             </div>
         </div>
@@ -64,5 +151,8 @@
     </div>
 </div>
 @include('layout.analytics')
+<script>
+    $('[data-toggle="popover"]').popover()
+</script>
 </body>
 </html>
