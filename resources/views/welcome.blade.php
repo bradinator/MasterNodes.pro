7668a8d3be0ae3e40b2c552e2a23ae27eaa81a58
Yoshi <jason@htmllabs.com>
2017-07-21 11:14:33 -0400

    Updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,54 +17,60 @@
         <div class="col-lg-1 hidden-md hidden-sm hidden-xs"></div>
     </div>
     <div class="row" style="margin-top: 50px;">
-        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
-        <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10" style="text-align: center;">
+        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12" style="text-align: center;">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
-                <a href="https://www.vultr.com/?ref=6877914"><img src="https://www.vultr.com/media/banner_1.png" width="728" height="90"></a>
+                <div class="row">
+                    <div class="span4"></div>
+                    <div class="span4"><a href="https://www.vultr.com/?ref=6877914"><span style="font-size: 16px">Best VPS Hosting for Masternodes</span><br><img src="https://www.vultr.com/media/banner_1.png" class="img-responsive center-block"></a></div>
+                    <div class="span4"></div>
+                </div>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
                 <br><br><br>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
-                <div class="alert alert-info">Want your Coin Listed here? Email us: <a href="mailto:addme@masternodes.pro">addme@masternodes.pro</a></div>
+                <div class="alert alert-info">Want your Coin Listed here? Email us:
+                    <a href="mailto:addme@masternodes.pro">addme@masternodes.pro</a> or join us on
+                    <a href="https://join.slack.com/t/masternodespro/shared_invite/MjEzNDg5NjM2NjI3LTE1MDAyNjE2ODgtZTQ0Y2M5ZDk5OQ"><i class="fa fa-slack" aria-hidden="true"></i>SLACK</a>
+                </div>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
-                <table width="100%">
-                    <thead>
-                    <tr>
-                        <td></td>
-                        <td>Coin</td>
-                        <td>Total Master Nodes</td>
-                        <td>Coins Locked in MasterNodes</td>
-                        <td>Current USD Price</td>
-                        <td>Required Coin's</td>
-                        <td>What's a Node Worth</td>
-                        <td>Daily Income</td>
-                        <td>Weekly Income</td>
-                        <td>Monthly Income</td>
-                        <td>Yearly Income</td>
-                        <td>Last Updated</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($coinList as $key => $one)
-                        <tr>
-                            <td><a href="https://{!! strtoupper($key) !!}.masternodes.pro" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></td>
-                            <td><a href="https://{!! strtoupper($key) !!}.masternodes.pro" target="_blank">{!! strtoupper($key) !!}</a></td>
-                            <td>{!! $one['totalMasterNodes'] !!}</td>
-                            <td>{!! number_format($one['totalMasterNodes'] * $one['masterNodeCoinsRequired'],'0','',',') !!}</td>
-                            <td>${!! number_format($one['currentUSDPrice'],2,'.','') !!}</td>
-                            <td>{!! $one['masterNodeCoinsRequired'] !!} {!! strtoupper($key) !!}</td>
-                            <td>${!! $one['masterNodeWorth'] !!}</td>
-                            <td>${!! $one['income']['daily'] !!}</td>
-                            <td>${!! $one['income']['weekly'] !!}</td>
-                            <td>${!! $one['income']['monthly'] !!}</td>
-                            <td>${!! $one['income']['yearly'] !!}</td>
-                            <td>{!! $one['lastUpdated'] !!}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                @foreach ($coinList as $key => $one)
+                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 align-left" style="margin-top: 20px;">
+                        <div style="border:2px solid #E4E6EB;border-radius: 10px;font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;min-width:285px;">
+                            <div>
+                                <div style="float:right;width:67%;border: 0px solid #000;text-align:center;padding:5px 0px;line-height:30px;">
+                                    <div>
+                                        <span style="font-size: 18px;">
+                                            <a href="https://{!! strtoupper($key) !!}.masternodes.pro" target="_blank" style="text-decoration: none; color: rgb(66, 139, 202);">{!! $one['name'] !!} ({!! strtoupper($key) !!})</a>
+                                        </span>
+                                    </div>
+                                    <div><span style="font-size: 16px;">${!! number_format($one['currentUSDPrice'],2,'.','') !!} USD</span></div>
+                                </div>
+                                <div style="text-align:center;padding:5px 0px;width:33%;"><a href="https://{!! strtoupper($key) !!}.masternodes.pro" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></div>
+                            </div>
+                            <div style="border-top: 1px solid #E4E6EB;clear:both;">
+                                <div style="text-align:center;float:left;width:50%;font-size:12px;padding:12px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Total Master Nodes <br><br> <span style="font-size: 17px; ">{!! $one['totalMasterNodes'] !!}</span></div>
+                                <div style="text-align:center;float:left;width:50%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Coins Locked <br><br> <span
+                                            style="font-size: 14px; ">{!! number_format($one['totalMasterNodes'] * $one['masterNodeCoinsRequired'],'0','',',') !!}</span></div>
+                            </div>
+                            <div style="border-top: 1px solid #E4E6EB;clear:both;">
+                                <div style="text-align:center;float:left;width:50%;font-size:12px;padding:12px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Required Coin's <br><br> <span style="font-size: 17px; ">{!! $one['masterNodeCoinsRequired'] !!}</span></div>
+                                <div style="text-align:center;float:left;width:50%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Node Worth <br><br> <span
+                                            style="font-size: 14px; ">${!! number_format($one['currentUSDPrice'] * $one['masterNodeCoinsRequired'],2,'.','') !!}</span></div>
+                            </div>
+                            <div style="border-top: 1px solid #E4E6EB;clear:both;">
+                                <div style="text-align:center;float:left;width:25%;font-size:12px;padding:12px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Daily <br><br> <span style="font-size: 17px; ">${!! $one['income']['daily'] !!}</span></div>
+                                <div style="text-align:center;float:left;width:25%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Weekly <br><br> <span style="font-size: 14px; ">${!! $one['income']['weekly'] !!}</span></div>
+                                <div style="text-align:center;float:left;width:25%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Monthly <br><br> <span style="font-size: 14px; ">${!! $one['income']['monthly'] !!}</span></div>
+                                <div style="text-align:center;float:left;width:25%;font-size:12px;padding:12px 0 16px 0;line-height:1.25em;"> Yearly <br><br> <span style="font-size: 14px; ">${!! $one['income']['yearly'] !!}</span></div>
+                            </div>
+                            <div style="border-top: 1px solid #E4E6EB;text-align:center;clear:both;font-size:10px;font-style:italic;padding:5px 0;">
+                                Last Updated <br><br> <span style="font-size: 14px; ">{!! $one['lastUpdated'] !!}</span>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
                 <br><br><br>
@@ -73,22 +79,23 @@
                 <div class="alert alert-info">Coming Soon.</div>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
-                <table width="100%">
-                    <thead>
-                    <tr>
-                        <td></td>
-                        <td>Coin</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($ComingSoonCoinList as $key => $one)
-                        <tr>
-                            <td><a href="{!! $one['url'] !!}" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></td>
-                            <td><a href="{!! $one['url'] !!}" target="_blank">{!! strtoupper($key) !!}</a></td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+
+                @foreach ($ComingSoonCoinList as $key => $one)
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 align-left" style="margin-top: 20px;">
+                    <div style="border:2px solid #E4E6EB;border-radius: 10px;font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;min-width:285px;">
+                        <div>
+                            <div style="float:right;width:67%;border: 0px solid #000;text-align:center;padding:5px 0px;line-height:30px;">
+                                <div>
+                                        <span style="font-size: 18px;">
+                                            <a href="{!! $one['url'] !!}" target="_blank" style="text-decoration: none; color: rgb(66, 139, 202);">{!! $one['name'] !!} ({!! strtoupper($key) !!})</a>
+                                        </span>
+                                </div>
+                            </div>
+                            <div style="text-align:center;padding:5px 0px;width:33%;"><a href="{!! $one['url'] !!}" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
                 <br><br><br>
@@ -97,45 +104,39 @@
                 <div class="alert alert-info">Help Fund Masternode Detail Site for 1 Year.</div>
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
-                <table width="100%">
-                    <thead>
-                    <tr>
-                        <td></td>
-                        <td>Coin</td>
-                        <td>Required</td>
-                        <td>Balance</td>
-                        <td>Need</td>
-                        <td>Donation Address</td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($donateCoinList as $key => $one)
-                        <tr>
-                            <td><a href="{!! $one['url'] !!}" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></td>
-                            <td><a href="{!! $one['url'] !!}" target="_blank">{!! strtoupper($key) !!}</a></td>
-                            <td>${!! number_format($one['need'],'2','.',',') !!}</td>
-                            <td>${!! number_format($one['current'],'2','.',',') !!}</td>
-                            <td>${!! number_format($one['balance'],'2','.',',') !!}</td>
-                            <td>
-                                @foreach ($one['donate'] as $donateKey => $donateOne)
-                                    {!! strtoupper($donateKey) !!}:
-                                    <a href="{!! strtoupper($donateKey) !!}:{!! $donateOne !!}"
-                                       target="_blank"
-                                       data-toggle="popover"
-                                       data-trigger="hover"
-                                       title="{!! strtoupper($donateKey) !!} address"
-                                       data-html="true"
-                                       data-content="<img src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={!! $donateOne !!}' width='150'>"
-                                    >
+
+                @foreach ($donateCoinList as $key => $one)
+                    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 align-left" style="margin-top: 20px;">
+                        <div style="border:2px solid #E4E6EB;border-radius: 10px;font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;min-width:285px;">
+                            <div>
+                                <div style="float:right;width:67%;border: 0px solid #000;text-align:center;padding:5px 0px;line-height:30px;">
+                                    <div>
+                                        <span style="font-size: 18px;">
+                                            <a href="{!! $one['url'] !!}" target="_blank" style="text-decoration: none; color: rgb(66, 139, 202);">{!! $one['name'] !!} ({!! strtoupper($key) !!})</a>
+                                        </span>
+                                    </div>
+                                    <div><span style="font-size: 16px;">Need ${!! number_format($one['balance'],'2','.',',') !!} USD</span></div>
+                                </div>
+                                <div style="text-align:center;padding:5px 0px;width:33%;"><a href="{!! $one['url'] !!}" target="_blank"><img src="{!! $one['logo'] !!}" width="50vmin"></a></div>
+                            </div>
+                            <div style="border-top: 1px solid #E4E6EB;clear:both;">
+                                <div style="text-align:center;float:left;width:50%;font-size:12px;padding:12px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Required <br><br> <span style="font-size: 17px; ">${!! number_format($one['need'],'2','.',',') !!}</span></div>
+                                <div style="text-align:center;float:left;width:50%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Balance <br><br> <span
+                                            style="font-size: 14px; ">${!! number_format($one['current'],'2','.',',') !!}</span></div>
+                            </div>
+                            @foreach ($one['donate'] as $donateKey => $donateOne)
+                            <div style="border-top: 1px solid #E4E6EB;text-align:center;clear:both;font-size:10px;font-style:italic;padding:5px 0;">
+                                {!! strtoupper($donateKey) !!} <br><br> <span style="font-size: 14px; ">
+                                    <a href="{!! strtoupper($donateKey) !!}:{!! $donateOne !!}" target="_blank"
+                                       data-toggle="popover" data-trigger="hover" title="{!! strtoupper($donateKey) !!} address"
+                                       data-html="true" data-content="<img src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={!! $donateOne !!}' width='150'>">
                                         {!! $donateOne !!}
-                                    </a>
-                                    <Br>
-                                @endforeach
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                                    </a></span>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @endforeach
             </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
                 <br><br><br>
@@ -144,7 +145,12 @@
                 <script data-cfasync=false src="//s.ato.mx/p.js#id=2194065&size=728x90"></script>
             </div>
         </div>
-        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12" style="text-align: center;">
+            <div class="col-lg-12 col-md-12 col-sm-9 col-xs-9" style="text-align: center;">
+                <a class="twitter-timeline" href="https://twitter.com/MasterNodesPro">Tweets by MasterNodesPro</a>
+                <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+        </div>
     </div>
     @include('layout.footer')
     <div class="modal fade" id="mainModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
