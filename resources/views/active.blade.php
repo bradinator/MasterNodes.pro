31403b9c7ae5c924d09fbc1f4b46ccff5278f2e4
Brad <c.b.cottrell@gmail.com>
2017-08-10 15:07:40 -0500

    CSS Fixes and cleanup

--- resources/views/active.blade.php
+++ resources/views/active.blade.php
@@ -12,32 +12,32 @@
     @include('layout.logo')
     <div class="row middle">
         <div class="col-lg-1 hidden-md hidden-sm hidden-xs"></div>
-        <div class="col-lg-10 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+        <div class="col-lg-10 col-md-12 col-sm-12 col-xs-12 text-center">
         </div>
         <div class="col-lg-1 hidden-md hidden-sm hidden-xs"></div>
     </div>
     <div class="row" style="margin-top: 50px;">
-        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12" style="text-align: center;">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 text-center" >
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
                 <div class="row">
                     <div class="span4"></div>
                     <div class="span4"><a href="https://www.vultr.com/?ref=6877914"><span style="font-size: 16px">Best VPS Hosting for Masternodes</span><br><img src="https://www.vultr.com/media/banner_1.png" class="img-responsive center-block"></a></div>
                     <div class="span4"></div>
                 </div>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 " >
                 <br><br><br>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
                 <div class="alert alert-info">Want your Coin Listed here? Email us:
                     <a href="mailto:addme@masternodes.pro">addme@masternodes.pro</a> or join us on
                     <a href="https://join.slack.com/t/masternodespro/shared_invite/MjEzNDg5NjM2NjI3LTE1MDAyNjE2ODgtZTQ0Y2M5ZDk5OQ"><i class="fa fa-slack" aria-hidden="true"></i>SLACK</a>
                 </div>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
                 @foreach ($coinList as $key => $one)
                     <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 align-left" style="margin-top: 20px;">
-                        <div style="border:2px solid #E4E6EB;border-radius: 10px;font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;min-width:285px;">
+                        <div class="coinbox-inner">
                             <div>
                                 <div style="float:right;width:67%;border: 0px solid #000;text-align:center;padding:5px 0px;line-height:30px;">
                                     <div>
@@ -51,28 +51,24 @@
                                 </div>
                                 <div style="text-align:center;padding:5px 0px;width:33%;"><a href="https://{!! strtoupper($one['coin']) !!}.masternodes.pro" target="_blank"><img src="{!! $one['logo'] !!}" width="50vw"></a></div>
                             </div>
-                            <div style="border-top: 1px solid #E4E6EB;text-align:center;clear:both;font-size:10px;font-style:italic;padding:5px 0;">
+                            <div class="coin-supply">
                                 Coin Supply: {!! $one['coin_supply'] !!}
                             </div>
-                            <div style="border-top: 1px solid #E4E6EB;clear:both;">
-                                <div style="text-align:center;float:left;width:50%;font-size:12px;padding:12px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Total Master Nodes <br><br> <span style="font-size: 17px; ">{!! $one['totalMasterNodes'] !!}</span></div>
-                                <div style="text-align:center;float:left;width:50%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Coins Locked <br><br> <span
-                                            style="font-size: 14px; ">{!! number_format($one['totalMasterNodes'] * $one['masterNodeCoinsRequired'],'0','',',') !!} ({!! number_format(((($one['totalMasterNodes'] * $one['masterNodeCoinsRequired']) /  $one['coin_supply'] ) * 100),'2','.',',') !!}%)<Br></span></div>
+                            <div class="coin-box-row text-center">
+                                <div class="col-md-6" > Total Master Nodes <br><br> <span>{!! $one['totalMasterNodes'] !!}</span></div>
+                                <div class="col-md-6" > Coins Locked <br><br> <spa >{!! number_format($one['totalMasterNodes'] * $one['masterNodeCoinsRequired'],'0','',',') !!} ({!! number_format(((($one['totalMasterNodes'] * $one['masterNodeCoinsRequired']) /  $one['coin_supply'] ) * 100),'2','.',',') !!}%)<Br></span></div>
                             </div>
-                            <div style="border-top: 1px solid #E4E6EB;clear:both;">
-                                <div style="text-align:center;float:left;width:33%;font-size:12px;padding:12px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Required Coin's <br><br> <span style="font-size: 17px; ">{!! $one['masterNodeCoinsRequired'] !!}</span></div>
-                                <div style="text-align:center;float:left;width:34%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Node Worth <br><br> <span
-                                            style="font-size: 14px; ">${!! number_format($one['currentUSDPrice'] * $one['masterNodeCoinsRequired'],2,'.','') !!}</span></div>
-                                <div style="text-align:center;float:left;width:33%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;">
-                                    ROI % <br><br> <span
-                                            style="font-size: 14px; ">{!!  number_format($one['roi'],2,'.','') !!}%</span>
+                            <div class="coin-box-row text-center">
+                                <div class="col-md-4" > Required Coin's <br><br> <span>{!! $one['masterNodeCoinsRequired'] !!}</span></div>
+                                <div class="col-md-4" > Node Worth <br><br> <span>${!! number_format($one['currentUSDPrice'] * $one['masterNodeCoinsRequired'],2,'.','') !!}</span></div>
+                                <div class="col-md-4">   ROI % <br><br> <span >{!!  number_format($one['roi'],2,'.','') !!}%</span>
                                 </div>
                             </div>
-                            <div style="border-top: 1px solid #E4E6EB;clear:both;">
-                                <div style="text-align:center;float:left;width:25%;font-size:12px;padding:12px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Daily <br><br> <span style="font-size: 17px; ">${!! number_format($one['income']['daily'],2,'.','') !!}</span></div>
-                                <div style="text-align:center;float:left;width:25%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Weekly <br><br> <span style="font-size: 14px; ">${!! number_format($one['income']['weekly'],2,'.','') !!}</span></div>
-                                <div style="text-align:center;float:left;width:25%;font-size:12px;padding:12px 0 16px 0;border-right:1px solid #E4E6EB;line-height:1.25em;"> Monthly <br><br> <span style="font-size: 14px; ">${!! number_format($one['income']['monthly'],2,'.','') !!}</span></div>
-                                <div style="text-align:center;float:left;width:25%;font-size:12px;padding:12px 0 16px 0;line-height:1.25em;"> Yearly <br><br> <span style="font-size: 14px; ">${!! number_format($one['income']['yearly'],2,'.','') !!}</span></div>
+                            <div class="coin-box-row text-center">
+                                <div class="col-md-3" > Daily <br><br> <span >${!! number_format($one['income']['daily'],2,'.','') !!}</span></div>
+                                <div class="col-md-3" > Weekly <br><br> <span >${!! number_format($one['income']['weekly'],2,'.','') !!}</span></div>
+                                <div class="col-md-3" > Monthly <br><br> <span >${!! number_format($one['income']['monthly'],2,'.','') !!}</span></div>
+                                <div class="col-md-3" > Yearly <br><br> <span >${!! number_format($one['income']['yearly'],2,'.','') !!}</span></div>
                             </div>
                             <div style="border-top: 1px solid #E4E6EB;text-align:center;clear:both;font-size:10px;font-style:italic;padding:5px 0;">
                                 Last Updated <br>{!! $one['lastUpdated'] !!}
@@ -81,15 +77,15 @@
                     </div>
                 @endforeach
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
                 <br><br><br>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="text-align: center;">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
                 <script data-cfasync=false src="//s.ato.mx/p.js#id=2194065&size=728x90"></script>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12" style="text-align: center;">
-            <div class="col-lg-12 col-md-12 col-sm-9 col-xs-9" style="text-align: center;">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center" >
+            <div class="col-lg-12 col-md-12 col-sm-9 col-xs-9 text-center">
                 <a class="twitter-timeline" href="https://twitter.com/MasterNodesPro">Tweets by MasterNodesPro</a>
                 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
